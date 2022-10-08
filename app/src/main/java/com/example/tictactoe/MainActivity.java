cc0c323f92be813b5461328f24f36f9890a716b7
Sam-Tech-2543 <mankarsarvesh2543@gmail.com>
2022-10-08 14:35:47 +0530

    Match Result Fixes

--- app/src/main/java/com/example/tictactoe/MainActivity.java
+++ app/src/main/java/com/example/tictactoe/MainActivity.java
@@ -4,13 +4,13 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
-import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.Toast;
 
+import java.util.Objects;
+
 public class MainActivity extends AppCompatActivity {
 
-    //Global Vars
     String playerName;
     Boolean gameState = true;
     int moves = 0;
@@ -22,7 +22,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        playerName = "X";
+        playerName = "X"; // First Player will be X
     }
 
     public void dropIn(View view) {
@@ -31,35 +31,39 @@ public class MainActivity extends AppCompatActivity {
         int t = Integer.parseInt(img.getTag().toString());
 
         if (board[t] == 0 && gameState) {
-            if (playerName == "X") {
+            if (Objects.equals(playerName, "X")) {
                 img.setImageResource(R.drawable.x);
-                playerName = "O";
-                board[t] = 1;
-                moves++;
+                board[t] = 1; // Marked Move of Player X
+
+                playerName = "O"; //Changed Player Name to O
             } else {
                 img.setImageResource(R.drawable.o);
-                playerName = "X";
-                board[t] = 2;
-                moves++;
+                board[t] = 2; // Marked Move of Player O
+
+                playerName = "X"; //Changed Player Name to X
             }
+            moves++;
             img.animate().alpha(1).setDuration(750);
         }
 
+        // Checking for the Winner
         for (int i = 0; i < 8; i++) {
             if (board[winningPos[i][0]] == 1 && board[winningPos[i][1]] == 1 && board[winningPos[i][2]] == 1) {
                 Toast.makeText(this, "Player X won the Game!", Toast.LENGTH_SHORT).show();
 
                 gameState = false;
             }
-            if (board[winningPos[i][0]] == 2 && board[winningPos[i][1]] == 2 && board[winningPos[i][2]] == 2) {
+            else if (board[winningPos[i][0]] == 2 && board[winningPos[i][1]] == 2 && board[winningPos[i][2]] == 2) {
                 Toast.makeText(this, "Player O won the Game!", Toast.LENGTH_SHORT).show();
 
                 gameState = false;
             }
-            if (moves == 9) {
-                Toast.makeText(this, "Game Drawn!", Toast.LENGTH_SHORT).show();
-                moves = 0;
-            }
+        }
+
+        // Game Drawn
+        if (moves == 9 && gameState) {
+            Toast.makeText(this, "Game Drawn!", Toast.LENGTH_SHORT).show();
+            moves = 0;
         }
 
     }
@@ -75,23 +79,23 @@ public class MainActivity extends AppCompatActivity {
         board[7] = 0;
         board[8] = 0;
 
-        ImageView img1 = (ImageView) findViewById(R.id.imageView1);
+        ImageView img1 = findViewById(R.id.imageView1);
         img1.animate().alpha(0);
-        ImageView img2 = (ImageView) findViewById(R.id.imageView2);
+        ImageView img2 = findViewById(R.id.imageView2);
         img2.animate().alpha(0);
-        ImageView img3 = (ImageView) findViewById(R.id.imageView3);
+        ImageView img3 = findViewById(R.id.imageView3);
         img3.animate().alpha(0);
-        ImageView img4 = (ImageView) findViewById(R.id.imageView4);
+        ImageView img4 = findViewById(R.id.imageView4);
         img4.animate().alpha(0);
-        ImageView img5 = (ImageView) findViewById(R.id.imageView5);
+        ImageView img5 = findViewById(R.id.imageView5);
         img5.animate().alpha(0);
-        ImageView img6 = (ImageView) findViewById(R.id.imageView6);
+        ImageView img6 = findViewById(R.id.imageView6);
         img6.animate().alpha(0);
-        ImageView img7 = (ImageView) findViewById(R.id.imageView7);
+        ImageView img7 = findViewById(R.id.imageView7);
         img7.animate().alpha(0);
-        ImageView img8 = (ImageView) findViewById(R.id.imageView8);
+        ImageView img8 = findViewById(R.id.imageView8);
         img8.animate().alpha(0);
-        ImageView img9 = (ImageView) findViewById(R.id.imageView9);
+        ImageView img9 = findViewById(R.id.imageView9);
         img9.animate().alpha(0);
 
         playerName = "X";
